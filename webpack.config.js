3c2e510abb7996247dc9b2b0bf387975aeab8bfd
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-19 12:20:07 +0200

    chore: Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const nodeExternals = require("webpack-node-externals");
+const path = require('path')
+
+const js = {
+    test: /\.js/,
+    exclude: /node_modules/,
+    use: {
+        loader: "babel-loader",
+        options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+    },
+};
+
+const serverConfig = {
+    mode: "development",
+    target: "node",
+    node: {
+        __dirname: false,
+    },
+    externals: [nodeExternals()],
+    entry: {
+        "index.js": path.resolve(__dirname, "src/index.js"),
+    },
+    module: {
+        rules: [js],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name]",
+    },
+};
+
+const clientConfig = {
+    mode: "development",
+    target: "web",
+    entry: {
+        "home.js": path.resolve(__dirname, "src/home.js"),
+    },
+    module: {
+        rules: [js],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/public"),
+        filename: "[name]",
+    },
+};
+
+module.exports = [serverConfig, clientConfig];
